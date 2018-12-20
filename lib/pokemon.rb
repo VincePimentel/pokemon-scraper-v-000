fd8f299dcc19d1790b5d113d4d3cc9b3f1ea05e5
Vince Andrew Pimentel <contact@vincep.me>
2018-12-20 05:38:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,6 +16,9 @@ class Pokemon
   def self.find(id, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
     self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
-    #binding.pry
+  end
+
+  def alter_hp
+
   end
 end
