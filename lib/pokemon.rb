7343b72b6e2c5b07f60a0fc62f6b29d2f072b3a7
Vince Andrew Pimentel <contact@vincep.me>
2018-12-20 05:55:12 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Pokemon
   attr_accessor :id, :name, :type, :hp, :db
 
@@ -16,7 +14,6 @@ class Pokemon
   def self.find(id, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
     self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
-    #binding.pry
   end
 
   def alter_hp(hp, db)
